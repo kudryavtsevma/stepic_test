17475b94b65f864e60f0b364ee4a11c35e43b59f
Maxim Kudryavtsev <59888764+kudryavtsevma@users.noreply.github.com>
2020-03-13 14:47:11 +0700

    Update test_items.py
    изспарвил название теста с "test_add_to_basker" на "test_add_to_basket"

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import pytest
 import time
 
 
-def test_add_to_basker(browser):
+def test_add_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     assert browser.find_element_by_css_selector('.btn-add-to-basket'), 'Кнопка не найдена'
